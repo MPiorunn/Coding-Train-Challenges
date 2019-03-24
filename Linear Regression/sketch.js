e6e53997a409bebb47f5e480e559dea08cfe390a
Maciej Piorun <maciej.piorun2@gmail.com>
2019-03-24 22:50:17 +0100

    Finished working on basic linear regression.

--- Linear Regression/sketch.js
+++ Linear Regression/sketch.js
@@ -1,3 +1,76 @@
+var data = [];
+var m = 1;
+var b = 0;
+
 function setup() {
+    createCanvas(400, 400);
+}
+
+function linearRegression() {
+    var xSum = 0;
+    var yYum = 0;
+    for (let i = 0; i < data.length; i++) {
+        xSum += data[i].x;
+        yYum += data[i].y;
+    }
+    var xMean = xSum / data.length;
+    var yMean = yYum / data.length;
+
+    var num = 0;
+    var den = 0;
+    for (let i = 0; i < data.length; i++) {
+        var x = data[i].x;
+        var y = data[i].y;
+        num += (x - xMean) * (y - yMean);
+        den += (x - xMean) * (x - xMean);
+    }
+    m = num / den;
+    b = yMean - m * xMean;
+}
+
+function drawLine() {
+    var x1 = 0;
+    var y1 = m * x1 + b;
+    var x2 = 1;
+    var y2 = m * x2 + b;
+
+    x1 = map(x1, 0, 1, 0, width);
+    y1 = map(y1, 0, 1, height, 0);
+    x2 = map(x2, 0, 1, 0, width);
+    y2 = map(y2, 0, 1, height, 0);
+    stroke(255, 0, 255);
+    line(x1, y1, x2, y2);
 
 }
+
+function mousePressed() {
+    var x = map(mouseX, 0, width, 0, 1);
+    var y = map(mouseY, 0, height, 1, 0);
+
+    var point = createVector(x, y,);
+    data.push(point);
+}
+
+function draw() {
+    background(51);
+    for (var i = 0; i < data.length; i++) {
+        var x = map(data[i].x, 0, 1, 0, width);
+        var y = map(data[i].y, 0, 1, height, 0);
+        fill(255);
+        stroke(255);
+        ellipse(x, y, 8, 8,);
+    }
+    if (data.length > 1) {
+        linearRegression();
+        drawLine()
+    }
+}
+
+/*
+
+Least squares :
+y = mx + b
+m = sum i = {0...n) [(xi - X)(yi - Y) / sum i = {0...n} [(xi - X)^2]
+X, Y - sum i= {0...n) (xi) / n
+b = Y - mX
+ */
